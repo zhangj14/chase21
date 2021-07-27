0932e10d33f1f3029a304bc7df524591303df5c9
zhangj14 <53502262+zhangj14@users.noreply.github.com>
2021-07-27 12:37:54 +1200

    deleted assignID, fixed caught form

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, url_for, request
 )
 
-from .db import *
+from app.db import get_db
 
 bp = Blueprint("forms", __name__, url_prefix="/forms")
 
@@ -10,32 +10,41 @@ bp = Blueprint("forms", __name__, url_prefix="/forms")
 def forms():
     return render_template("forms/all_forms.html")
 
-@bp.route("/caught", methods=["GET", "POST"])
+@bp.route("/caught", methods=("GET", "POST"))
 def caught():
     if request.method == "POST":
-        chaser_id = request.form["chaser_id"]
+        chaser_id = request.form['chaser_id']
         runner_id = request.form['runner_id']
-        valid = True
-        db, cursor = get_db()
-        error = None
+        valid = False
+        db, cursor = get_db() # get database connection
+        message = None
+
         if not chaser_id:
-            error = "Chaser ID is required."
+            message = "Chaser ID is required."
         elif not runner_id:
-            error = "Runner ID is required"
+            message = "Runner ID is required."
         else:
+            # select the person's runner_id with corresponding chaser_id
             cursor.execute(f"SELECT runner_id FROM all_players \
-                       WHERE chaser_id = '{chaser_id}' LIMIT 1")
-            if cursor.fetchone() != runner_id:
-                valid = False
-                error = "Please double check your Chaser ID and Runner ID."
-            cursor.execute(f"INSERT INTO caught (chaser_id, runner_id, valid) \
-                       values ('{chaser_id}', '{runner_id}', {valid})")
-        if error is None:
+                            WHERE chaser_id = '{chaser_id}' LIMIT 1")
+            db_runner_id = cursor.fetchone()
+
+        if not db_runner_id:
+            message = "Invalid chaser ID."
+        elif db_runner_id[0] != runner_id:
+            message = "Please double check your Chaser ID and Runner ID."
+        if message is None:
+            valid = True
             cursor.execute(f"UPDATE all_players SET game_status = 'dead' \
-                       WHERE chaser_id = {runner_id}")
+                            WHERE chaser_id = '{runner_id}'")
+            message = "Congratulations on catching your runner."
+        cursor.execute(f"INSERT INTO caught (chaser_id, runner_id, valid) \
+                    values ('{chaser_id}', '{runner_id}', {valid})")
+        db.commit()
+        flash(message)
     return render_template("forms/caught.html")
 
-@bp.route("/opt_out", methods=["GET", "POST"])
+@bp.route("/opt_out", methods=("GET", "POST"))
 def op_out():
     if request.method == "POST":
         email = request.form["email"]
@@ -44,10 +53,10 @@ def op_out():
         db.execute(f"INSERT INTO opt_out (email, reasons) VALUES ({email}, {reasons})")
     return render_template("forms/opt_out.html")
 
-@bp.route("/reassign", methods=["GET", "POST"])
+@bp.route("/reassign", methods=("GET", "POST"))
 def reassign():
     return render_template("forms/reassign.html")
 
-@bp.route("/report", methods=["GET", "POST"])
+@bp.route("/report", methods=("GET", "POST"))
 def report():
     return render_template("forms/report.html")
