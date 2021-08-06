b5f8c764e7c30ba4efece5c3c6c261069f1c5c7d
zhangj14 <53502262+zhangj14@users.noreply.github.com>
2021-08-06 16:55:01 +1200

    removed wrong schema file

--- app/forms.py
+++ app/forms.py
@@ -15,34 +15,27 @@ def caught():
     if request.method == "POST":
         chaser_id = request.form["chaser_id"]
         runner_id = request.form['runner_id']
+        year_level = request.form['year_level']
         valid = True
-        db, cursor = get_db()
+        db = get_db().cursor()
         error = None
         if not chaser_id:
             error = "Chaser ID is required."
         elif not runner_id:
             error = "Runner ID is required"
         else:
-            cursor.execute(f"SELECT runner_id FROM all_players \
-                       WHERE chaser_id = '{chaser_id}' LIMIT 1")
-            if cursor.fetchone() != runner_id:
+            db.execute("SELECT runner_id FROM year{} WHERE game_id = %s".format(year_level), (chaser_id))
+            if db.fetchone() != runner_id:
                 valid = False
-                error = "Please double check your Chaser ID and Runner ID."
-            cursor.execute(f"INSERT INTO caught (chaser_id, runner_id, valid) \
-                       values ('{chaser_id}', '{runner_id}', {valid})")
+                error = "Please double check your year level, Chaser ID and Runner ID."
+            db.execute("INSERT INTO caught (chaser_id, runner_id, valid) values (%s, %s, %s)", (chaser_id, runner_id, valid))
         if error is None:
-            cursor.execute(f"UPDATE all_players SET game_status = 'dead' \
-                       WHERE chaser_id = {runner_id}")
+            db.execute("")
     return render_template("forms/caught.html")
 
-@bp.route("/opt_out", methods=["GET", "POST"])
+@bp.route("/op_out", methods=["GET", "POST"])
 def op_out():
-    if request.method == "POST":
-        email = request.form["email"]
-        reasons = request.form["reasons"]
-        db = get_db().cursor()
-        db.execute(f"INSERT INTO opt_out (email, reasons) VALUES ({email}, {reasons})")
-    return render_template("forms/opt_out.html")
+    return render_template("forms/op_out.html")
 
 @bp.route("/reassign", methods=["GET", "POST"])
 def reassign():
