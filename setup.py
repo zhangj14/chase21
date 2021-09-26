992ec5ed43c2d7bbbe812038b9593475f9bfeac6
zhangj14 <53502262+zhangj14@users.noreply.github.com>
2021-09-26 17:36:48 +1300

    updated setup and requirement

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='chaser_2',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
